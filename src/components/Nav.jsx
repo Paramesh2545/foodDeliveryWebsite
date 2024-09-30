e24f0963afafb732fa1b5b8f2a4f058c498a1ad7
Paramesh2545 <152502158+Paramesh2545@users.noreply.github.com>
2024-09-30 20:26:25 +0530

    last

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -42,7 +42,11 @@ const Nav = () => {
   return (
     <div className="navmain">
       <div className="logo">
-        <img src="src/assets/logo-photoroom-photoroom.png" alt="logo" className="logoimg" />
+        <img
+          src="src/assets/heavenlyBites_logo.png"
+          alt="logo"
+          className="logoimg"
+        />
       </div>
       <div className="links-container">
         {small && (
@@ -72,13 +76,7 @@ const Nav = () => {
                 textDecoration: "none",
               }}
             >
-              <h3
-                className={
-                  darkMode ? "navelementsText dark" : "navelementsText"
-                }
-              >
-                Home
-              </h3>
+              <h3 className="navelementsText">Home</h3>
             </NavLink>
           </div>
           <div className="navitems second">
@@ -89,13 +87,7 @@ const Nav = () => {
                 textDecoration: "none",
               }}
             >
-              <h3
-                className={
-                  darkMode ? "navelementsText dark" : "navelementsText"
-                }
-              >
-                NearBy
-              </h3>
+              <h3 className="navelementsText">NearBy</h3>
             </NavLink>
           </div>
           <div className="navitems second">
@@ -106,13 +98,7 @@ const Nav = () => {
                 textDecoration: "none",
               }}
             >
-              <h3
-                className={
-                  darkMode ? "navelementsText dark" : "navelementsText"
-                }
-              >
-                Maps
-              </h3>
+              <h3 className="navelementsText">Maps</h3>
             </NavLink>
           </div>
           <div className="navitems third">
@@ -123,13 +109,7 @@ const Nav = () => {
                 textDecoration: "none",
               }}
             >
-              <h3
-                className={
-                  darkMode ? "navelementsText dark" : "navelementsText"
-                }
-              >
-                Cart
-              </h3>
+              <h3 className="navelementsText">Cart</h3>
             </NavLink>
           </div>
           <div className="navitems fourth">
@@ -140,13 +120,7 @@ const Nav = () => {
                 textDecoration: "none",
               }}
             >
-              <h3
-                className={
-                  darkMode ? "navelementsText dark" : "navelementsText"
-                }
-              >
-                Profile
-              </h3>
+              <h3 className="navelementsText">Profile</h3>
             </NavLink>
           </div>
         </div>
@@ -156,34 +130,9 @@ const Nav = () => {
         <CiLogout />
         {hamClicked && <p className="text">Logout</p>}
       </div> */}
-      <div className={small ? "theme small" : "theme"}>
+      {/* <div className={small ? "theme small" : "theme"}>
         <Theme />
-        {auth.currentUser ? (
-          <button className="Btn" onClick={logout}>
-            <div className="sign">
-              <svg viewBox="0 0 512 512">
-                <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
-              </svg>
-            </div>
-            <div className="Logtext">Logout</div>
-          </button>
-        ) : (
-          <NavLink
-            className="cta"
-            key={auth.currentUser}
-            to="/login"
-            style={{
-              textDecoration: "none",
-            }}
-          >
-            <span>Login/signup</span>
-            <svg width="15px" height="10px" viewBox="0 0 13 10">
-              <path d="M1,5 L11,5"></path>
-              <polyline points="8 1 12 5 8 9"></polyline>
-            </svg>
-          </NavLink>
-        )}
-      </div>
+      </div> */}
     </div>
   );
 };
